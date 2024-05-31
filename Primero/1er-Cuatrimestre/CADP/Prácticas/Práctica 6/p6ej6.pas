2ba36645918a90b9dcbc3231f5210d0c8c5b7b19
bela <132023278+bBelkiss@users.noreply.github.com>
2024-05-31 10:56:07 -0300

    Fixed procedures

--- Primero/1er-Cuatrimestre/CADP/Prácticas/Práctica 6/p6ej6.pas
+++ Primero/1er-Cuatrimestre/CADP/Prácticas/Práctica 6/p6ej6.pas
@@ -102,18 +102,16 @@ begin
 end;
 procedure leerSonda(var s:sonda);
 begin
-    with s do begin
         writeln('Nombre:');
-        readln(nombre);
+        readln(s.nombre);
         writeln('Duración estimada de la misión:');
-        readln(duracEstimada);
+        readln(s.duracEstimada);
         writeln('Costo de construcción:');
-        readln(costoConstruc);
+        readln(s.costoConstruc);
         writeln('Costo de mantenimiento mensual:');
-        readln(costoMantenMens);
+        readln(s.costoMantenMens);
         writeln('Rango del espectro electromagnetico:');
-        readln(rango);
-    end;
+        readln(s.rango);
 end;
 procedure generarLista(var pri:lista;var promedio:real; costoPromedio:real);
 var
