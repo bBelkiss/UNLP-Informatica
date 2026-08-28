859cc6e6442e4f3b18c1777b9901d1494c38286c
Bela <josefinabid@gmail.com>
2026-08-28 11:52:08 -0300

    🌱 upload example for tstg

--- 1_Primero/2_Semestre/ARQUITECTURA/Practicas/Practica1/p1ej1.asm
+++ 1_Primero/2_Semestre/ARQUITECTURA/Practicas/Practica1/p1ej1.asm
@@ -2,4 +2,23 @@
 ; de veces que la letra ‘a’ (minúscula) aparece en MENSAJE y lo almacene en la variable CANT. Por ejemplo, si 
 ; MENSAJE contiene “Hola, Buenas Tardes”, entonces CANT debe valer 3.
 
-ORG 1000H;
+ORG 1000H
+MSG DB "hola buenas"
+LETRA DB 'a'
+CANT DB ?
+
+ORG 2000H
+MOV BX, OFFSET MSG
+MOV AL, OFFSET LETRA - OFFSET MSG
+MOV DL, LETRA
+MOV DH, 0
+SEGUIR: MOV AH, [BX]
+        CMP AH, DL
+        JNZ SALTO
+        INC DH
+SALTO: INC BX
+       DEC AL
+       JNZ SEGUIR
+MOV CANT, DH
+HLT
+END
